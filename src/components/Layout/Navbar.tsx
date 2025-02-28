e7d8f80203d0f1d0ddb9dc91c4d6279811c5b3d2
langbart <lorenzo.longobardi@gmail.com>
2025-02-28 19:36:18 +0100

    Update Navbar brand text to PESKAS with surveys validation subtitle

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
         </button>
         <h1 className="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
           <a href=".">
-            Validation Portal
+            PESKAS | <span className="text-muted">surveys validation</span>
           </a>
         </h1>
         <div className="navbar-nav flex-row order-md-last">
